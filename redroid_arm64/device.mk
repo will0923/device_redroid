f3b5253e1743c3b65028d6de44f3e9c9ad71585d
Will Fang <wancong_fang@fzzixun.com>
2023-01-12 14:49:19 +0800

    remove MindTheGapps

--- redroid_arm64/device.mk
+++ redroid_arm64/device.mk
@@ -2,4 +2,3 @@ PRODUCT_COPY_FILES += \
     device/redroid/mediacodec.policy.arm:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
 
 $(call inherit-product, device/redroid-prebuilts/prebuilts_arm.mk)
-$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
